be5c159d095ebbfc6c93af2dc31cf33216082f79
BGorson <BGorson32@gmail.com>
2019-04-07 12:29:12 -0500

    added shooting stars

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -8,79 +8,9 @@ function myFunction() {
   }
 }
 
-var star = document.getElementsByClassName("star")
-var starAlt = document.getElementsByClassName('star2')
 
-setInterval(function(){
-  document.getElementsByClassName("star").style.left= Math.floor(Math.random() * 50)+ 10
-  document.getElementsByClassName('star2').style.left= Math.floor(Math.random() * 50)+ 10
- }, 1000);
 
-anime({
-  targets: '.star',
-  translateX: 480,
-  translateY: 200,
-  opacity:[1,0],
-  scaleY: [
-    { value: [1.75, 1], duration: 10 },
-    { value: -1, duration: 100, delay: 150, easing: 'easeInElastic' },
 
-  ],
-  scaleX: [
-    { value: [-1.75, 1], duration: 10 },
-    { value: 2, duration: 100, delay: 150, easing: 'easeOutElastic' },
-  ],
-  loop: true,
-  easing: 'easeInOutElastic',
-  loopBegin: function(anim){ 
-    anime({
-      targets:'.star',
-      easing: 'easeInOutElastic',
-      opactiy:[1]
-    })
-  },
-  loopComplete: function(anim){
-   anime({
-     targets:'.star',
-     easing: 'easeInOutElastic',
-     opactiy:[0]
-   })
-  },
-  delay: anime.stagger(1100) 
-});
-
-anime({
-  targets: '.star2',
-  translateX: 480,
-  translateY: 200,
-  opacity:[1,0],
-  scaleY: [
-    { value: [1.75, 1], duration: 10 },
-    { value: -1, duration: 100, delay: 150, easing: 'easeInElastic' },
-
-  ],
-  scaleX: [
-    { value: [-1.75, 1], duration: 10 },
-    { value: 2, duration: 100, delay: 150, easing: 'easeOutElastic' },
-  ],
-  loop: true,
-  easing: 'easeInOutElastic',
-  loopBegin: function(anim){ 
-    anime({
-      targets:'.star',
-      easing: 'easeInOutElastic',
-      opactiy:[1]
-    })
-  },
-  loopComplete: function(anim){
-   anime({
-     targets:'.star',
-     easing: 'easeInOutElastic',
-     opactiy:[0]
-   })
-  },
-  delay: anime.stagger(1600) 
-});
 
 // When the user scrolls the page, execute myFunction 
 window.onscroll = function() {myFunctionSticky()};
